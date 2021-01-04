b682f6de6b45b0e04ddfc2559ee3aec0e5f0f723
willy chang <willy.chang@oakda.io>
2021-01-04 14:05:50 +0800

    update deploy rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :repo_url, 'git@github.com:willyjie23/hotwire.git'
 # Deploy to the user's home directory
 set :deploy_to, "/home/deploy/#{fetch :application}"
 
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads', 'assets/javascripts/libraries'
 
 # Only keep the last 5 releases to save disk space
 set :keep_releases, 5
