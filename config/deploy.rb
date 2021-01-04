d99a97fba769cda4102d5650a1d975c55b1bf213
willy chang <willy.chang@oakda.io>
2021-01-04 11:34:24 +0800

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,13 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.14.1"
 
-set :application, "hotwire"
+set :application, 'hotwire'
 set :repo_url, "git@github.com:willyjie23/hotwire.git"
+
+# Deploy to the user's home directory
 set :deploy_to, "/home/deploy/#{fetch :application}"
+
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+
+# Only keep the last 5 releases to save disk space
 set :keep_releases, 5
