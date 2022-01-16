5a6c0a81d13593846472b06f08240211e3eecc68
willychang <a0903084107@gmail.com>
2022-01-16 20:59:03 +0800

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.16.0"
+lock "~> 3.15.0"
 
 set :application, 'hotwire'
 set :repo_url, 'git@github.com:willyjie23/hotwire.git'
