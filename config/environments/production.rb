7eb0613e57cfd5b0f6f4ed65147822ef7ea7bb1b
willychang <a0903084107@gmail.com>
2022-02-08 11:39:59 +0800

    add cable url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://willychang.xyz/cable'
+  config.action_cable.url = 'ws://willychang.xyz/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
