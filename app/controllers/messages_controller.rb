417e6fff44ffffd41a604c9ce482819ef9b059cb
willychang <a0903084107@gmail.com>
2022-02-11 10:40:18 +0800

    modify

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -26,6 +26,7 @@ class MessagesController < ApplicationController
 
     respond_to do |format|
       if @message.save
+        format.html
         format.json { render :show, status: :created, location: @message }
       else
         format.html { render :new }
